aa6ff1ef277b991b2217b4922e9e0790abfa4dab
Aaron Nolan <aaron@Aarons-MacBook-Pro.local>
2024-05-20 16:12:01 -0500

    add ga4

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
+
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './lib/amplify-config.ts';
+
 import { ThemeProvider } from './components/context/theme-context.tsx';
 import { TooltipProvider } from './components/ui/tooltip.tsx';
 import { Toaster } from '@/components/ui/toaster';
+
+if (import.meta.env.VITE_GA_MEASUREMENT_ID) {
+  ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
